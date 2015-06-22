ce995e3e9fdb372c3d69aca8f6d40cf3b783b027
policecar <priska@23bit.net>
2015-06-22 18:11:01 -0400

    Make pep8 compatible.

--- Theano/VariationalAutoencoder.py
+++ Theano/VariationalAutoencoder.py
@@ -1,17 +1,20 @@
+import numpy as np
+import theano as th
+import theano.tensor as T
+
 """
+Authors:
 Joost van Amersfoort - <joost.van.amersfoort@gmail.com>
 Otto Fabius - <ottofabius@gmail.com
 
 License: MIT
 """
 
-import numpy as np
-import theano as th
-import theano.tensor as T
-
-"""This class implements an auto-encoder with Variational Bayes"""
 
 class VA:
+
+    """ This class implements an auto-encoder with Variational Bayes. """
+
     def __init__(self, HU_decoder, HU_encoder, dimX, dimZ, batch_size, L=1, learning_rate=0.01):
         self.HU_decoder = HU_decoder
         self.HU_encoder = HU_encoder
@@ -27,120 +30,124 @@ class VA:
 
         self.continuous = False
 
-
     def initParams(self):
         """Initialize weights and biases, depending on if continuous data is modeled an extra weight matrix is created"""
-        W1 = np.random.normal(0,self.sigmaInit,(self.HU_encoder,self.dimX))
-        b1 = np.random.normal(0,self.sigmaInit,(self.HU_encoder,1))
+        W1 = np.random.normal(0, self.sigmaInit, (self.HU_encoder, self.dimX))
+        b1 = np.random.normal(0, self.sigmaInit, (self.HU_encoder, 1))
 
-        W2 = np.random.normal(0,self.sigmaInit,(self.dimZ,self.HU_encoder))
-        b2 = np.random.normal(0,self.sigmaInit,(self.dimZ,1))
+        W2 = np.random.normal(0, self.sigmaInit, (self.dimZ, self.HU_encoder))
+        b2 = np.random.normal(0, self.sigmaInit, (self.dimZ, 1))
 
-        W3 = np.random.normal(0,self.sigmaInit,(self.dimZ,self.HU_encoder))
-        b3 = np.random.normal(0,self.sigmaInit,(self.dimZ,1))
-        
-        W4 = np.random.normal(0,self.sigmaInit,(self.HU_decoder,self.dimZ))
-        b4 = np.random.normal(0,self.sigmaInit,(self.HU_decoder,1))
+        W3 = np.random.normal(0, self.sigmaInit, (self.dimZ, self.HU_encoder))
+        b3 = np.random.normal(0, self.sigmaInit, (self.dimZ, 1))
 
-        W5 = np.random.normal(0,self.sigmaInit,(self.dimX,self.HU_decoder))
-        b5 = np.random.normal(0,self.sigmaInit,(self.dimX,1))
+        W4 = np.random.normal(0, self.sigmaInit, (self.HU_decoder, self.dimZ))
+        b4 = np.random.normal(0, self.sigmaInit, (self.HU_decoder, 1))
+
+        W5 = np.random.normal(0, self.sigmaInit, (self.dimX, self.HU_decoder))
+        b5 = np.random.normal(0, self.sigmaInit, (self.dimX, 1))
 
         if self.continuous:
-            W6 = np.random.normal(0,self.sigmaInit,(self.dimX,self.HU_decoder))
-            b6 = np.random.normal(0,self.sigmaInit,(self.dimX,1))
-            self.params = [W1,W2,W3,W4,W5,W6,b1,b2,b3,b4,b5,b6]
+            W6 = np.random.normal(
+                0, self.sigmaInit, (self.dimX, self.HU_decoder))
+            b6 = np.random.normal(0, self.sigmaInit, (self.dimX, 1))
+            self.params = [W1, W2, W3, W4, W5, W6, b1, b2, b3, b4, b5, b6]
         else:
-            self.params = [W1,W2,W3,W4,W5,b1,b2,b3,b4,b5]
+            self.params = [W1, W2, W3, W4, W5, b1, b2, b3, b4, b5]
 
         self.h = [0.01] * len(self.params)
 
-
-    def initH(self,miniBatch):
+    def initH(self, miniBatch):
         """Compute the gradients and use this to initialize h"""
         totalGradients = self.getGradients(miniBatch)
         for i in range(len(totalGradients)):
-            self.h[i] += totalGradients[i]*totalGradients[i]
+            self.h[i] += totalGradients[i] * totalGradients[i]
 
     def createGradientFunctions(self):
-        #Create the Theano variables
-        W1,W2,W3,W4,W5,W6,x,eps = T.dmatrices("W1","W2","W3","W4","W5","W6","x","eps")
+        # Create the Theano variables
+        W1, W2, W3, W4, W5, W6, x, eps = T.dmatrices(
+            "W1", "W2", "W3", "W4", "W5", "W6", "x", "eps")
 
-        #Create biases as cols so they can be broadcasted for minibatches
-        b1,b2,b3,b4,b5,b6 = T.dcols("b1","b2","b3","b4","b5","b6")
+        # Create biases as cols so they can be broadcasted for minibatches
+        b1, b2, b3, b4, b5, b6 = T.dcols("b1", "b2", "b3", "b4", "b5", "b6")
 
         if self.continuous:
-            h_encoder = T.nnet.softplus(T.dot(W1,x) + b1)
-        else:   
-            h_encoder = T.tanh(T.dot(W1,x) + b1)
-
-        mu_encoder = T.dot(W2,h_encoder) + b2
-        log_sigma_encoder = 0.5*(T.dot(W3,h_encoder) + b3)
-
-        #Find the hidden variable z
-        z = mu_encoder + T.exp(log_sigma_encoder)*eps
-
-        prior = 0.5* T.sum(1 + 2*log_sigma_encoder - mu_encoder**2 - T.exp(2*log_sigma_encoder))
-
-
-        #Set up decoding layer
-        if self.continuous:
-            h_decoder = T.nnet.softplus(T.dot(W4,z) + b4)
-            mu_decoder = T.nnet.sigmoid(T.dot(W5,h_decoder) + b5)
-            log_sigma_decoder = 0.5*(T.dot(W6,h_decoder) + b6)
-            logpxz = T.sum(-(0.5 * np.log(2 * np.pi) + log_sigma_decoder) - 0.5 * ((x - mu_decoder) / T.exp(log_sigma_decoder))**2)
-            gradvariables = [W1,W2,W3,W4,W5,W6,b1,b2,b3,b4,b5,b6]
+            h_encoder = T.nnet.softplus(T.dot(W1, x) + b1)
         else:
-            h_decoder = T.tanh(T.dot(W4,z) + b4)
-            y = T.nnet.sigmoid(T.dot(W5,h_decoder) + b5)
-            logpxz = -T.nnet.binary_crossentropy(y,x).sum()
-            gradvariables = [W1,W2,W3,W4,W5,b1,b2,b3,b4,b5]
+            h_encoder = T.tanh(T.dot(W1, x) + b1)
 
+        mu_encoder = T.dot(W2, h_encoder) + b2
+        log_sigma_encoder = 0.5 * (T.dot(W3, h_encoder) + b3)
+
+        # Find the hidden variable z
+        z = mu_encoder + T.exp(log_sigma_encoder) * eps
+
+        prior = 0.5 * \
+            T.sum(1 + 2 * log_sigma_encoder - mu_encoder **
+                  2 - T.exp(2 * log_sigma_encoder))
+
+        # Set up decoding layer
+        if self.continuous:
+            h_decoder = T.nnet.softplus(T.dot(W4, z) + b4)
+            mu_decoder = T.nnet.sigmoid(T.dot(W5, h_decoder) + b5)
+            log_sigma_decoder = 0.5 * (T.dot(W6, h_decoder) + b6)
+            logpxz = T.sum(-(0.5 * np.log(2 * np.pi) + log_sigma_decoder) -
+                           0.5 * ((x - mu_decoder) / T.exp(log_sigma_decoder))**2)
+            gradvariables = [W1, W2, W3, W4, W5, W6, b1, b2, b3, b4, b5, b6]
+        else:
+            h_decoder = T.tanh(T.dot(W4, z) + b4)
+            y = T.nnet.sigmoid(T.dot(W5, h_decoder) + b5)
+            logpxz = -T.nnet.binary_crossentropy(y, x).sum()
+            gradvariables = [W1, W2, W3, W4, W5, b1, b2, b3, b4, b5]
 
         logp = logpxz + prior
 
-        #Compute all the gradients
-        derivatives = T.grad(logp,gradvariables)
+        # Compute all the gradients
+        derivatives = T.grad(logp, gradvariables)
 
-        #Add the lowerbound so we can keep track of results
+        # Add the lowerbound so we can keep track of results
         derivatives.append(logp)
 
-        self.gradientfunction = th.function(gradvariables + [x,eps], derivatives, on_unused_input='ignore')
-        self.lowerboundfunction = th.function(gradvariables + [x,eps], logp, on_unused_input='ignore')
+        self.gradientfunction = th.function(
+            gradvariables + [x, eps], derivatives, on_unused_input='ignore')
+        self.lowerboundfunction = th.function(
+            gradvariables + [x, eps], logp, on_unused_input='ignore')
 
     def iterate(self, data):
         """Main method, slices data in minibatches and performs an iteration"""
-        [N,dimX] = data.shape
-        batches = np.arange(0,N,self.batch_size)
+        [N, dimX] = data.shape
+        batches = np.arange(0, N, self.batch_size)
         if batches[-1] != N:
-            batches = np.append(batches,N)
+            batches = np.append(batches, N)
 
-        for i in range(0,len(batches)-2):
-            miniBatch = data[batches[i]:batches[i+1]]
+        for i in range(0, len(batches) - 2):
+            miniBatch = data[batches[i]:batches[i + 1]]
             totalGradients = self.getGradients(miniBatch.T)
-            self.updateParams(totalGradients,N,miniBatch.shape[0])
+            self.updateParams(totalGradients, N, miniBatch.shape[0])
 
-    def getLowerBound(self,data):
+    def getLowerBound(self, data):
         """Use this method for example to compute lower bound on testset"""
         lowerbound = 0
-        [N,dimX] = data.shape
-        batches = np.arange(0,N,self.batch_size)
+        [N, dimX] = data.shape
+        batches = np.arange(0, N, self.batch_size)
         if batches[-1] != N:
-            batches = np.append(batches,N)
+            batches = np.append(batches, N)
 
-        for i in range(0,len(batches)-2):
-            miniBatch = data[batches[i]:batches[i+1]]
-            e = np.random.normal(0,1,[self.dimZ,miniBatch.shape[0]])
-            lowerbound += self.lowerboundfunction(*(self.params),x=miniBatch.T,eps=e)
+        for i in range(0, len(batches) - 2):
+            miniBatch = data[batches[i]:batches[i + 1]]
+            e = np.random.normal(0, 1, [self.dimZ, miniBatch.shape[0]])
+            lowerbound += self.lowerboundfunction(*
+                                                  (self.params), x=miniBatch.T, eps=e)
 
-        return lowerbound/N
+        return lowerbound / N
 
-
-    def getGradients(self,miniBatch):
+    def getGradients(self, miniBatch):
         """Compute the gradients for one minibatch and check if these do not contain NaNs"""
         totalGradients = [0] * len(self.params)
         for l in range(self.L):
-            e = np.random.normal(0,1,[self.dimZ,miniBatch.shape[1]])
-            gradients = self.gradientfunction(*(self.params),x=miniBatch,eps=e)
+            e = np.random.normal(0, 1, [self.dimZ, miniBatch.shape[1]])
+            gradients = self.gradientfunction(
+                *(self.params), x=miniBatch, eps=e)
             self.lowerbound += gradients[-1]
 
             for i in range(len(self.params)):
@@ -148,13 +155,15 @@ class VA:
 
         return totalGradients
 
-    def updateParams(self,totalGradients,N,current_batch_size):
+    def updateParams(self, totalGradients, N, current_batch_size):
         """Update the parameters, taking into account AdaGrad and a prior"""
         for i in range(len(self.params)):
-            self.h[i] += totalGradients[i]*totalGradients[i]
+            self.h[i] += totalGradients[i] * totalGradients[i]
             if i < 5 or (i < 6 and len(self.params) == 12):
-                prior = 0.5*self.params[i]
+                prior = 0.5 * self.params[i]
             else:
                 prior = 0
 
-            self.params[i] += self.learning_rate/np.sqrt(self.h[i]) * (totalGradients[i] - prior*(current_batch_size/N))
+            self.params[i] += self.learning_rate / \
+                np.sqrt(
+                    self.h[i]) * (totalGradients[i] - prior * (current_batch_size / N))
--- Theano/demo_trainmnist.py
+++ Theano/demo_trainmnist.py
@@ -1,15 +1,3 @@
-"""
-Authors: 
-Joost van Amersfoort - <joost.van.amersfoort@gmail.com>
-Otto Fabius - <ottofabius@gmail.com>
-
-#License: MIT
-"""
-
-"""This script trains an auto-encoder on the MNIST dataset and keeps track of the lowerbound"""
-
-#python demo_trainmnist.py -s mnist.npy
-
 import VariationalAutoencoder
 import numpy as np
 import argparse
@@ -17,20 +5,35 @@ import time
 import gzip
 import pickle
 
+"""
+This script trains an auto-encoder on the MNIST dataset and keeps track of the lowerbound.
+
+Authors:
+Joost van Amersfoort - <joost.van.amersfoort@gmail.com>
+Otto Fabius - <ottofabius@gmail.com>
+
+Modifications by priska
+
+License: MIT
+
+Usage: python demo_trainmnist.py -s mnist.npy
+"""
+
+""""""
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-d","--double", help="Train on hidden layer of previously trained AE - specify params", default = False)
+parser.add_argument("-d", "--double", help="Train on hidden layer of previously trained AE - specify params", default=False)
 
 args = parser.parse_args()
 
 print("Loading MNIST data")
-#Retrieved from: http://deeplearning.net/data/mnist/mnist.pkl.gz
+# Retrieved from: http://deeplearning.net/data/mnist/mnist.pkl.gz
 
 f = gzip.open('../mnist.pkl.gz', 'rb')
 # work-around to deal with incompatibilities between pickle for Python 2.x and Python 3.x
 u = pickle._Unpickler(f)
 u.encoding = 'latin1'
 (x_train, t_train), (x_valid, t_valid), (x_test, t_test) = u.load()
-# (x_train, t_train), (x_valid, t_valid), (x_test, t_test) = pickle.load(f)
 f.close()
 
 data = x_train
@@ -46,11 +49,11 @@ learning_rate = 0.01
 if args.double:
     print('computing hidden layer to train new AE on')
     prev_params = np.load(args.double)
-    data = (np.tanh(data.dot(prev_params[0].T) + prev_params[5].T) + 1) /2
-    x_test = (np.tanh(x_test.dot(prev_params[0].T) + prev_params[5].T) +1) /2
+    data = (np.tanh(data.dot(prev_params[0].T) + prev_params[5].T) + 1) / 2
+    x_test = (np.tanh(x_test.dot(prev_params[0].T) + prev_params[5].T) + 1) / 2
 
-[N,dimX] = data.shape
-encoder = VariationalAutoencoder.VA(HU_decoder,HU_encoder,dimX,dimZ,batch_size,L,learning_rate)
+[N, dimX] = data.shape
+encoder = VariationalAutoencoder.VA(HU_decoder, HU_encoder, dimX, dimZ, batch_size, L, learning_rate)
 
 
 if args.double:
@@ -70,11 +73,9 @@ for j in range(1500):
     print('Iteration:', j)
     encoder.iterate(data)
     end = time.time()
-    print(("Iteration %d, lower bound = %.2f,"
-          " time = %.2fs"
-          % (j, encoder.lowerbound/N, end - begin)))
+    print(("Iteration %d, lower bound = %.2f, time = %.2fs" % (j, encoder.lowerbound / N, end - begin)))
     begin = end
 
     if j % 5 == 0:
         print("Calculating test lowerbound")
-        testlowerbound = np.append(testlowerbound,encoder.getLowerBound(x_test))
+        testlowerbound = np.append(testlowerbound, encoder.getLowerBound(x_test))
